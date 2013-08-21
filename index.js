535c536ee6fa7320a95fb1fb97724fcdded3b976
Dominic Tarr <dominic.tarr@gmail.com>
2013-08-21 22:31:25 +0100

    fixed view port for mobile

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 #! /usr/bin/env node
 
+if(process.stdin.isTTY) {
+  console.error('USAGE: browserify client.js | indexhtmlify > index.html')
+  process.exit(1)
+}
+
 var log = console.log
 log('<!DOCTYPE html>')
 log('<html>')
-log('<head><meta charset=utf-8></head>')
+log('<head>')
+log('<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />')
+log('<meta charset=utf-8></head>')
 log('<body></body>')
 log('<script language=javascript>')
 process.stdin.pipe(process.stdout)
