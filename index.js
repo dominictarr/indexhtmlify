b5d952890a0812704d4db63a710928dcc83b12e4
Dominic Tarr <dominic.tarr@gmail.com>
2013-10-15 16:51:08 -0700

    add --style option

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
 
+var opts = require('optimist').argv
+
 if(process.stdin.isTTY) {
-  console.error('USAGE: browserify client.js | indexhtmlify > index.html')
+  console.error('USAGE: browserify client.js | indexhtmlify --style style.css > index.html')
   process.exit(1)
 }
 
@@ -11,6 +13,13 @@ log('<html>')
 log('<head>')
 log('<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />')
 log('<meta charset=utf-8></head>')
+
+if(opts.style) {
+  log('<style>')
+  log(require('fs').readFileSync(opts.style, 'utf8')))
+  log('</style>')
+}
+
 log('<body></body>')
 log('<script language=javascript>')
 process.stdin.pipe(process.stdout)
