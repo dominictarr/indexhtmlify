c734ac05c47a255203378dd0cc2b365f865e1873
Raynos <raynos2@gmail.com>
2014-05-03 10:51:01 -0700

    80 char limit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 var opts = require('optimist').argv
 
 if(process.stdin.isTTY) {
-  console.error('USAGE: browserify client.js | indexhtmlify --style style.css > index.html')
+  console.error('USAGE: browserify client.js | indexhtmlify ' +
+    '--style style.css > index.html')
   process.exit(1)
 }
 
@@ -11,7 +12,8 @@ var log = console.log
 log('<!DOCTYPE html>')
 log('<html>')
 log('<head>')
-log('<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />')
+log('<meta content="width=device-width, initial-scale=1.0, ' +
+    'maximum-scale=1.0, user-scalable=0" name="viewport" />')
 log('<meta charset=utf-8></head>')
 
 if(opts.style) {
